27343749b0309c412692ef01c83caf9b243eac1b
ali moniri <alimoniri81@gmail.com>
2022-05-27 11:47:00 +0430

    finish main right container

--- js/app.js
+++ js/app.js
@@ -4,3 +4,18 @@ function openMenu() {
     menu.classList.toggle("open-menu")
 
 }
+
+// input focus -----------------------
+
+
+function focusBox() {
+    let mailBlock = document.forms[0]
+    mailBlock.style.border = "3px solid #F9B500"
+    mailBlock.style.borderRadius = "5px"
+}
+
+function blurBox() {
+    let mailBlock = document.forms[0]
+    mailBlock.style.border = "1px solid #F9B500"
+    mailBlock.style.borderRadius = "0px"
+}
